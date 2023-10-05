b54b44d951fe12e6650f95e7b4447245c2458e35
Mr-i-me <Mr-i-me@Ivans-MacBook-Pro.local>
2023-10-05 14:21:36 -0300

    feat: update list

--- src/App/Pages/Serasa/List/List.js
+++ src/App/Pages/Serasa/List/List.js
@@ -1,39 +1,61 @@
 import React, { useEffect, useState } from "react";
 import { DataStore } from "@aws-amplify/datastore";
-import { Container, Button, Row, Col, Card, Badge } from "react-bootstrap";
+import { Badge, Button, Card, Col, Container, Row } from "react-bootstrap";
 import { SerasaReport } from "../../../../models";
 import { useNavigate } from "react-router-dom";
 import { Collection } from "@aws-amplify/ui-react";
-import { FaTrash, FaEye } from "react-icons/fa"; // Importing icons
+import { FaEye } from "react-icons/fa"; // Importing icons
+
+const Header = () => {
+  return (
+    <header className="py-4">
+      <Container>
+        <Row>
+          <Card>
+            <h2 className="">Relatórios SERASA</h2>
+          </Card>
+        </Row>
+      </Container>
+    </header>
+  );
+};
 
 const ListHeader = () => {
   return (
-      <Row className="border-top border-bottom py-3 bg-light text-uppercase font-weight-bold">
-        <Col>#</Col>
-        <Col>Document Number</Col>
-        <Col>Type</Col>
-        <Col>Status</Col>
-        <Col>Actions</Col>
-      </Row>
+    <Row className="border-top border-bottom py-3 bg-light text-uppercase font-weight-bold">
+      <Col>#</Col>
+      <Col>Document Number</Col>
+      <Col>Type</Col>
+      <Col>Status</Col>
+      <Col>Actions</Col>
+    </Row>
   );
 };
 
 const ListItem = ({ model, index, handleDelete, navigate }) => {
   return (
-      <Row className="border-bottom py-2 hover:bg-light">
-        <Col>{index + 1}</Col>
-        <Col>{model.documentNumber}</Col>
-        <Col>{model.type}</Col>
-        <Col><Badge variant={model.status === 'Active' ? 'success' : 'danger'}>{model.status}</Badge></Col>
-        <Col>
-          <Button variant="danger" onClick={() => handleDelete(model.id)} className="mr-2">
-            <FaTrash /> Delete
-          </Button>
-          <Button variant="info" onClick={() => navigate(`${model.id}`)}>
-            <FaEye /> View
-          </Button>
-        </Col>
-      </Row>
+    <Row className="border-bottom py-2">
+      <Col>{index + 1}</Col>
+      <Col>{model.documentNumber}</Col>
+      <Col>{model.type}</Col>
+      <Col>
+        <Badge
+          variant={model.status === "Active" ? "success" : "danger"}
+          className="py-2"
+        >
+          {model.status}
+        </Badge>
+      </Col>
+      <Col>
+        <Button
+          variant="info"
+          className={"btn btn-sm btn-outline-light"}
+          onClick={() => navigate(`${model.id}`)}
+        >
+          <FaEye /> View
+        </Button>
+      </Col>
+    </Row>
   );
 };
 
@@ -65,15 +87,14 @@ const List = (props) => {
   };
 
   return (
-      <Container>
-        <Row className="mb-4">
-          <Col>
-            <h1>Fetched Serasa Reports</h1>
-          </Col>
-        </Row>
-        <Card>
-          <Card.Body>
-            <Collection
+    <div>
+      <Header />
+
+      <main>
+        <Container>
+          <Card>
+            <Card.Body>
+              <Collection
                 isPaginated
                 isSearchable
                 items={models}
@@ -81,17 +102,19 @@ const List = (props) => {
                 direction="column"
                 gap="20px"
                 wrap="nowrap"
-            >
-              {(model, index) => (
+              >
+                {(model, index) => (
                   <>
-                    {index === 0 && (<ListHeader />)}
+                    {index === 0 && <ListHeader />}
                     <ListItem {...{ model, index, handleDelete, navigate }} />
                   </>
-              )}
-            </Collection>
-          </Card.Body>
-        </Card>
-      </Container>
+                )}
+              </Collection>
+            </Card.Body>
+          </Card>
+        </Container>
+      </main>
+    </div>
   );
 };
 
